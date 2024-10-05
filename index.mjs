49c13eb8fc3a8883427a9999ab2b7ba8753014dc
Dasun Piyumal <w.piyumal2319@gmail.com>
2024-10-05 22:43:05 +0530

    Initial database connection

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
