955f92323687cf72ffe05bc6335883b18397f216
Rashmika Rathnayaka <rashmikarathnayaka01@gmail.com>
2024-10-27 01:02:53 +0530

    add index to username

--- utilities/database/ddl.sql
+++ utilities/database/ddl.sql
@@ -791,3 +791,29 @@ END //
 DELIMITER ;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#indexes for the tables
+-- Create index on Username for faster lookup
+CREATE INDEX idx_username ON customer (Username);
