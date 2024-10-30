872017fa185b761fa75ddb55969c0c8ebdd3fc85
Dasun Piyumal <w.piyumal2319@gmail.com>
2024-10-30 20:21:41 +0530

    Create 9_events.sql

---
+++ utilities/database/definition_files/9_events.sql
@@ -0,0 +1,20 @@
+DELIMITER //
+
+CREATE EVENT ResetCompletedHours
+    ON SCHEDULE
+        EVERY 1 WEEK
+            STARTS '2024-10-27 00:00:00'  -- Set to a Monday
+    DO
+    BEGIN
+        -- Reset CompletedHours for drivers
+        UPDATE driver
+        SET CompletedHours = '00:00:00';
+
+        -- Reset CompletedHours for assistants
+        UPDATE assistant
+        SET CompletedHours = '00:00:00';
+    END;
+//
+
+
+DELIMITER ;
